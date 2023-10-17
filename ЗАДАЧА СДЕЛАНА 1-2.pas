93701883be2c8a8d3fe3c86e6cf8d469f162735f
Danil2227 <144226545+Danil2227@users.noreply.github.com>
2023-10-17 14:55:56 +0300

    Add files via upload

---
+++ ЗАДАЧА СДЕЛАНА 1-2.pas
@@ -0,0 +1,25 @@
+﻿program ab;
+
+var
+  x, flag, a: real;
+
+begin
+  x := -9;
+  while x <= -2 do
+  begin
+    if x < -7 then
+      a := Exp(x) - ln(x)
+    else if -4 <= x then
+      a := 98
+    else if  x < -4 then 
+      a := ln(x) * x - x;
+    if a = a then 
+      flag := 1
+    else flag := 0;
+    begin
+      if flag = 1 then writeln('x=', x, ' ', 'y= ', a:0:4)
+      else writeln('x=', x, ' ', 'Такого нет');
+      x := x + 0.3;
+    end;
+  end;
+end.
